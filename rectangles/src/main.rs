ff0bd785f2b74ab6c6e2f7c4f6d6a6d37df6e7dc
uda5150 <tks.udagawa@gmail.com>
2019-03-06 01:52:13 +0900

    構造体のデバック

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,12 +1,27 @@
+
+// 構造体
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// 借用
+// 所有権を保って、rect1を使用し続けるために関数シグネチャと関数呼び出し時に&を使っている
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!(
+        "rect1 is {:#?}", rect1
+    );
 
     println!(
         "The area of the rectangles is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
