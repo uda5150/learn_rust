b0153aee60ba667f8aa06584abbc80a066113a70
uda5150 <tks.udagawa@gmail.com>
2019-03-14 01:20:20 +0900

    add追加

--- string_update/src/main.rs
+++ string_update/src/main.rs
@@ -15,9 +15,26 @@ fn main() {
     let s4 = String::from("world!");
 
     // s3が借用できないのは、型のせいか？
+    // addの定義がseldの所有権を奪うかららしい
     // s3はここで破棄される
     let s5 = s3 + &s4; 
     // println!("{}", s3);
     println!("{}", s4);
     println!("{}", s5);
+
+    let s6 = String::from("tic");
+    let s7 = String::from("tac");
+    let s8 = String::from("toe");
+
+    let s9 = s6 + "-" + &s7 + "-" + &s8;
+
+    println!("{}", s9);
+
+    let s10 = String::from("tic");
+    let s11 = String::from("tac");
+    let s12 = String::from("toe");
+
+    let s13 = format!("{}-{}-{}", s10, s11, s12);
+
+    println!("{}", s13);
 }
